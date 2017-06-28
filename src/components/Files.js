bad6a51b07a88ec40b0cecb473df9c8dbd42dd4d
Deepak Jacob <jacob.deepak@yahoo.com>
2017-06-28 09:40:03 +0100

    ux

--- src/components/Files.js
+++ src/components/Files.js
@@ -14,11 +14,21 @@ const Files = ({ files, paginationLinks }) =>
     <p />
     <Link to="/files/sort/name/asc/1"> sort by name asc </Link> |
     <Link to="/files/sort/name/desc/1"> sort by name desc </Link>
+
     {files.map((file, index) =>
-      <div key={index}>
-        {file.name}
+
+      <div key={index} className="filelist">
+        <span>File id: {file.id}</span> 
+        <span>File name: {file.name}</span> 
+        <span>File desc: {file.desc}</span> 
+        <span>File size: {file.name}</span> 
+        <span>File size: {file.ext}</span> 
+        <span>File size: {file.created}</span> 
       </div>
+
     )}
+
+
     <div>{paginationLinks.map(renderPaginationLink)}</div>
   </div>;
 
