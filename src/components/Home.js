8a6d2051642ab7064a88d7e14892e42dbbd4a9ce
Deepak Jacob <jacob.deepak@yahoo.com>
2017-06-29 10:13:15 +0100

    update params

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Home = () => <NavLink to="/files">My Files</NavLink>;
+const Home = () => <NavLink to="/files?_page=1">My Files</NavLink>;
 
 export default Home;
